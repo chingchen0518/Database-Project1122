b254fb6d53846c171cf4f1f29026968538e23b64
陳彥友 <2791163@gmail.com>
2024-05-29 18:56:52 +0800

    123

--- djangoProjecttest222/urls.py
+++ djangoProjecttest222/urls.py
@@ -33,14 +33,14 @@ urlpatterns = [
     path('login/login_act/', my_app.views.login_act, name='login_act'),
     path('logout/', my_app.views.logout, name='logout'),
 
-    #房屋顯示
+    # 房屋顯示
     path('house_list/',my_app.views.house_list, name='house_lists'),
     path('house_rent_cont/<str:hId>',my_app.views.house_rent_cont),
     path('house_rent/<str:hId>', my_app.views.house_rent, name='house_rent'),
     path('house_list_sold/',my_app.views.house_list_sold, name='house_lists_sold'),
     path('house_sold/<str:hId>', my_app.views.house_sold, name='house_sold'),
 
-    #新增刪除
+    # 新增刪除
     path('upload_page',my_app.views.upload_page,name='upload_page'),
     path('upload_page/add_house',my_app.views.add_house, name='add_house'),
     path('upload_page/sold', my_app.views.upload_page_sold, name='upload_page_sold'),
@@ -49,24 +49,24 @@ urlpatterns = [
     path('delete_house/<str:hId>/', my_app.views.delete_house,name='delete_house'),
     path('delete_browse', my_app.views.delete_browse, name='delete_browse'),
 
-    #用戶中心
+    # 用戶中心
     path('account_center/', my_app.views.account_center, name="account_center"),
 
-    #沒用的東西
+    # 沒用的東西
     path('testing/',my_app.views.testing,name="testing"),
 
-    #搜尋測試
+    # 搜尋測試
     path('house_list/search_test/', my_app.views.search_test, name='search_test'),
 
-    #編輯
+    # 編輯
     path('edit_page/<str:hId>/',my_app.views.edit_page_show,name='edit_page'),
     path('edit_page/edit_house/<str:hId>/',my_app.views.edit_page_update, name='edit_house'),
     path('edit_page_sold/<str:hId>/', my_app.views.edit_page_show_sold, name='edit_page_sold'),
     path('edit_page_sold/edit_house/<str:hId>/', my_app.views.edit_page_update_sold, name='edit_house_sold'),
 
-    #Templates
+    # Templates
 
-    #其他功能
+    # 其他功能
     # path('comment_test', my_app.views.comment_test),
     path('add_comment/<str:hId>/', my_app.views.add_comment, name='add_comment'),
     path('upload/', my_app.views.upload_image, name='upload_image'),
@@ -78,7 +78,7 @@ urlpatterns = [
     path('add_appointment/<str:hId>/', my_app.views.add_appointment, name='add_appointment'),
     path('accept_booking/<int:booking_seq>', my_app.views.accept_booking, name='accept_booking'),
     path('reject_booking/<int:booking_seq>', my_app.views.reject_booking, name='reject_booking'),
-    path('renew_booking/<int:booking_seq>', my_app.views.renew_booking, name='renew_booking'),
+    # path('renew_booking/<int:booking_seq>', my_app.views.renew_booking, name='renew_booking'),
     path('renew_booking_time/<int:booking_seq>', my_app.views.renew_booking_time, name='renew_booking_time'),
 
     path('update_user_detail/', my_app.views.update_user_detail, name='update_user_detail'),
